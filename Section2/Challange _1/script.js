d508e2ac7d4cb544a719504d9a735cc847ea1b47
mads nielsen kjerumgaard <mads@mmm-m.dk>
2021-03-31 11:37:25 +0200

    uploading clean code

--- Section2/Challange _1/script.js
+++ Section2/Challange _1/script.js
@@ -1,33 +1,16 @@
     /*
-    Coding challange 1
-    */
+Coding challange 1
+*/
 
-    /*
-    Mark and John are trying to compare their BMI, which is calculated using the formula:
-    BMI=mass/height^2 = mass / (height * height)
-    (mass is kg, and height is meters)
+/*
+Mark and John are trying to compare their BMI, which is calculated using the formula:
+BMI=mass/height^2 = mass / (height * height)
+(mass is kg, and height is meters)
 
-    1. Store marks and johns mass and height in variables
-    2. Calculate both their BMI
-    3. Create a boolean variable containing information about wheater mark has a heigher BMI
-    than john
-    4. Print a string to the console contaning the variable from step 3
-    */
-
-    var _MarkMass, _MarkHeight, _JohnMass, _JohnHeight, _MarkBmi, _JohnBmi, _MarkHeigherThanJohn;
-     _MarkMass = 79; //kg
-     _MarkHeight = 1.80 //centimeters
-     _JohnMass = 90 //kg
-     _JohnHeight = 2.00 //centimeters
-
-     _MarkBmi = _MarkMass  / (_MarkHeight * _MarkHeight);
-     _JohnBmi = _JohnMass / (_JohnHeight * _JohnHeight);
-
-     console.log(_MarkBmi);
-     console.log(_JohnBmi);
-
-     _MarkHeigherThanJohn = _MarkBmi > _JohnBmi;
-
-     console.log(_MarkHeigherThanJohn);
-     console.log("Is Mark´s BMI higher than John's?: " + _MarkHeigherThanJohn);
+1. Store marks and johns mass and height in variables
+2. Calculate both their BMI
+3. Create a boolean variable containing information about wheater mark has a heigher BMI
+than john
+4. Print a string to the console contaning the variable from step 3
+*/
 
--- Section2/Challange_2/script.js
+++ Section2/Challange_2/script.js
@@ -25,31 +25,3 @@ Mary
 3. Then change the score to show different winners. Rember to take in account a draw
 */
 
-var _JohnAvg, _MikAvg, _MaryAvg;
-//_JohnAvg = (89 + 120 + 103) / 3;
-//_MaryAvg = (97 + 134 + 105) / 3;
-//_MikAvg = (116 + 94 + 123) / 3;
-_MikAvg = (116 + 94 + 123) / 3;
-_JohnAvg = (89 + 120 + 103) / 3;
-_MaryAvg = (97 + 134 + 105) / 3;
-
-
-if (_JohnAvg > _MikAvg && _JohnAvg > _MaryAvg) { //john
-    console.log("John has the highest avager");
-} else if ( _MikAvg >_JohnAvg && _MikAvg > _MaryAvg) { //Mike
-    console.log("Mike has the higest avager");
-} else if (_MaryAvg > _JohnAvg && _MaryAvg > _MikAvg){ //Mary
-    console.log("Mary has the highest avager");
-} else if (_JohnAvg === _MikAvg && _JohnAvg >_MaryAvg) {
-    console.log("John and Mike has a draw");
-} else if (_JohnAvg > _MikAvg && _JohnAvg === _MaryAvg){
-    console.log("John and Mary has a draw");
-} else if (_MikAvg === _JohnAvg && _MikAvg >_MaryAvg) {
-    console.log("Mike and John has a draw");
-} else if (_MikAvg > _JohnAvg && _MikAvg === _MaryAvg){
-    console.log("Mike and Mary has a draw")
-} else if (_MaryAvg === _JohnAvg && _MaryAvg > _MikAvg) {
-    console.log("Mary and John has a draw");
-} else if (_MaryAvg > _JohnAvg && _MaryAvg === _MikAvg){
-    console.log("Mike and Mary has a draw")
-} 
